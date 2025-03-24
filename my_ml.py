bcbf99d19d150325675ed2585101f7311aeb5b48
Zee-glitch <zeeshanrnk786@gmail.com>
2025-03-24 23:11:05 +0530

    added ml funcs

--- my_ml.py
+++ my_ml.py
@@ -1,1 +1,11 @@
 print("all my ml models going implement here")
+
+
+def model1():
+    print("model1 is implemented")
+
+def model2():
+    print("model2 is implemented")
+
+def model3():
+    print("model3 is implemented")
